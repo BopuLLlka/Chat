93cf7f9a1d1f9b4093569c12d06aacbb7850d333
Sergey Davydov <sergey@deskalerts.com>
2023-04-08 12:02:09 +0300

    get and post sample

--- Chat.Client.Console/Program.cs
+++ Chat.Client.Console/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello 2, New World!");
+
+
+using System.Text.Json;
+
+var httpClient = new HttpClient();
+
+Console.WriteLine("Enter your name:");
+
+var name = Console.ReadLine() ?? throw new ArgumentException("Name is null!");
+
+Console.WriteLine("Enter your age:");
+
+var age = Console.ReadLine() ?? throw new ArgumentException("Age is null!");
+
+
+var clinet = new Client {
+    Name = name,
+    Age = age,
+};
+
+var jsonString = JsonSerializer.Serialize(clinet);
+
+StringContent postData = new StringContent(jsonString);
+
+var postResult = await httpClient.PostAsync($"https://localhost:7047/HelloPost", postData);
+var postcontent = await postResult.Content.ReadAsStringAsync();
+
+Console.WriteLine(postcontent);
+
+
+public class Client 
+{   
+    public string Name { get; set; }
+    public string Age { get; set; }
+}
