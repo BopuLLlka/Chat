7e6121b1a0712584d1ce08b903cfe7b7f5b8709f
Sergey Davydov <sergey@deskalerts.com>
2023-10-06 18:55:38 +0300

    signalR

--- Chat.Front/scripts/main.js
+++ Chat.Front/scripts/main.js
@@ -3,3 +3,72 @@ function ready() {
 }
 
 document.addEventListener("DOMContentLoaded", ready);
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("https://wjwddj3d-7047.euw.devtunnels.ms/hub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+
+        await SendMessage("hello");
+
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
+
+
+async function SendMessage(messageText){
+    try {
+        let user = new User(1, "qwe");
+        let message = new Message(1, messageText, 1,2, true);
+
+        console.log("send message to user: ");
+        console.log(user);
+        console.log("send message: ");
+        console.log(message);
+        await connection.invoke("SendMessage", user, message);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+connection.on("ReceiveMessage", (user, message) => {
+    console.log(user);
+    console.log(message);
+});
+
+
+const User = class {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+  };
+
+
+const Message = class {
+    constructor(id, text, senderId, recipientId,isEveryone) {
+        this.id = id;
+        this.text = text;
+        this.senderId = senderId;
+        this.recipientId = recipientId;
+        this.isEveryone = isEveryone;
+    }
+};
+
+
+
+// Start the connection.
+start();
+
+
--- Chat.Server/Program.cs
+++ Chat.Server/Program.cs
@@ -1,11 +1,30 @@
 using Chat.Server.SignalR;
 
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://127.0.0.1:5500/")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+});
+
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapGet("/", () => "Hello World!");
 app.MapHub<MainHub>("/hub");
+
+
 
 app.Run();
